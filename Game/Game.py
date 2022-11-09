ead5117a2622ca736cd0994803552acd4428a58d
Namulove <jinubaek@naver.com>
2022-11-09 15:50:05 +0900

    2022.11.09

--- Game/Game.py
+++ Game/Game.py
@@ -12,8 +12,10 @@ class player:
         self.battle_def = battle_def
     def player_atk(self, enemy):
         enemy.hp -= self.atk
+        print('===================')
         print('{}의 피해를 준다?'.format(self.atk))
-        print('적은 체력이 {}만 남았다'.format(enemy.hp))
+        print('적은 체력이 {} 남았다'.format(enemy.hp))
+        print('===================')
     def player_def(self):
         self.battle_def = self.deff * 2
     def battle_gain(self, enemy):
@@ -46,7 +48,7 @@ while True:
     print('나는 누구인가?')
     time.sleep(0.5)
     print('레벨을 올리고 보스를 격파하여 원래 세상으로 돌아가세요?')
-    p1 = player(10, 8, 5, 0, 0, 1, 0)
+    p1 = player(30, 5, 5, 0, 0, 1, 0)
     level1 = level(10, 1)
 
 
@@ -95,44 +97,48 @@ while True:
                 print('우다다다!')
                 self.hp = 0
             else:
-                print('==보스 차례==')
+                print('==========보스 차례==========')
                 p.hp -= self.atk + p.battle_def
                 print('{}의 피해를 받았다.?'.format(self.atk))
                 print('나의 체력은 {}만 남았다?'.format(p.hp))
+                print('====================')
 
     def battle(enemy, player):
         while True:
-            player_battle_decision = input('==================\n(공격-1)-(방어-2)\n숫자를 입력하세요>>>')
+            print('\n====================\n플레이어\n체력: {}-공격력: {}-방어력: {}\n===================='.format(player.hp, player.atk, player.deff))
+            player_battle_decision = input('\n(공격-1)-(방어-2)-(탐색-3)\n숫자를 입력하세요>>>')
             if player_battle_decision == '1':
                 player.player_atk(enemy)
             elif player_battle_decision == '2':
-                player.player_def(player)
-            enemy.boss_atk(enemy, player)
+                player.player_def()
+            elif player_battle_decision == '3':
+                print('\n====================\n보스\n체력: {}-공격력: {}\n===================='.format(enemy.hp, enemy.atk))
+            enemy.boss_atk(player)
             if player.hp <= 0:
                 print('너의 패배')
-            else:
+                break
+            if enemy.hp <= 0:
                 print('승리')
-                player.battle_gain(player,enemy)
-
-
-
-
-
-
-
-
+                player.battle_gain(enemy)
+                level1.level_up(player)
+                break
 
     quiz1 = quiz('손톱은 발톱보다 더 빨리 자란다?', '네', False)
     quiz2 = quiz('몸의 북기를 방치하면 살이 된다?', '아니오', False)
-    quest1 = quest('치즈', 10, 10)
-    quest2 = quest('빵', 10, 10)
-    boss1 = boss(10, 2, 10, 10)
+    quest1 = quest('치즈', 2, 5)
+    quest2 = quest('빵', 2, 5)
+    boss1 = boss(20, 8, 5, 20)
+    boss2 = boss(30, 6, 30, 30)
     while True:
         move = input('===================\n어떤 행동을 하실 건가요?\n(이동-1)-(상태 확인-2)-()\n숫자를 입력하세요>>>')
+        time.sleep(0.5)
+
         if move == '1':
             travel = True
             while travel:
                 reply = input('===================\n어디로 이동하실건가요?\n(던전 퀘스트-1)-(보스방-2)-(뒤로가기-0)\n숫자를 입력하세요>>>')
+                time.sleep(0.5)
+
                 if reply == '1':
                     while True:
                         reply_quest = input('===================\n퀘스트를 선택하세요?\n(치즈 퀘스트-1)-(빵집 퀘스트-2)-(뒤로가기-0)\n숫자를 입력하세요>>>')
@@ -146,14 +152,18 @@ while True:
                             break
                         if reply_quest == '0':
                             break
+
                 elif reply == '2':
                     while True:
-                        reply_boss = input('===================\n보스를 선택하세요?\n(보스1-1)-(뒤로가기-0)\n숫자를 입력하세요>>>')
+                        reply_boss = input('===================\n보스를 선택하세요?\n(던전1-1)-(뒤로가기-0)\n숫자를 입력하세요>>>')
+                        time.sleep(0.5)
                         if reply_boss == '1':
-                            boss1.boss_atk(p1)
+                            time.sleep(0.5)
+                            battle(boss1, p1)
                             travel = False
                             break
                         if reply_boss == '0':
+                            time.sleep(0.5)
                             break
                 elif reply == '0':
                     travel = False
@@ -167,3 +177,7 @@ while True:
                     break
                 else:
                     self_check = input('다시 입력해주세요\n>>>')
+
+        if p1.hp <= 0:
+            print('플레이어: 깨꼬닭!\n======================\n당신은 사망했습니다\n======================')
+            break
